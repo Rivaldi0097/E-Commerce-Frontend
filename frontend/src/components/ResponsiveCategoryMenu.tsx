2d0f44608632f2bc097dfcc1c17fc704b6c1dcdc
Catharina Anggono <79496983+catharinaanggono@users.noreply.github.com>
2023-10-05 00:06:01 +0800

    Implemented product by category on responsive navbar

--- frontend/src/components/ResponsiveCategoryMenu.tsx
+++ frontend/src/components/ResponsiveCategoryMenu.tsx
@@ -1,15 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import { useGetProductCategoriesQuery } from "../redux/productSlice";
 import "../styles/responsiveCategoryMenu.css";
 
-function ResponsiveCategoryMenu() {
+interface IResponsiveCategoryMenu {
+  showCategory: boolean;
+  setShowCategory: React.Dispatch<React.SetStateAction<boolean>>;
+  setMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function ResponsiveCategoryMenu({
+  showCategory,
+  setShowCategory,
+  setMenu,
+}: IResponsiveCategoryMenu) {
   const { data: categoriesData, isSuccess: categoriesSuccess } =
     useGetProductCategoriesQuery([]);
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (categoryName: string) => {
+    setShowCategory(false);
+    navigate(`/category/${categoryName}`);
+    setMenu(false);
+  };
 
   return (
     <div className="category__menu__responsive">
       {categoriesData?.map((categoryName: string) => {
         return (
-          <div className="category__items__responsive" key={categoryName}>
+          <div
+            className="category__items__responsive"
+            key={categoryName}
+            onClick={() => {
+              handleCategoryClick(categoryName);
+            }}
+          >
             {categoryName}
           </div>
         );
